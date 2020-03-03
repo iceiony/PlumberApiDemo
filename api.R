0f95ecb33075c7eda493eb54463292e38b258287
Adrian Ionita <iceiony@yahoo.com>
2020-03-03 18:37:25 +0000

    add exmaple for outputting dataframes as downloadable csv@

--- api.R
+++ api.R
@@ -1,7 +1,6 @@
 #* Generate a histogram of random numbers
-#* @png
-#* @get /my_plot
-function(){
-    rand <- rnorm(100)
-    hist(rand)
+#* @serializer csv
+#* @get /generate
+generator <- function(){
+    iris
 }
--- start_api.R
+++ start_api.R
@@ -1,4 +1,26 @@
 library(plumber)
+library(readr)
+library(dplyr)
+
+serializer_csv <- function(){
+  function(val, req, res, errorHandler){
+    tryCatch({
+      res$setHeader("Content-Type", "text/csv")
+      res$setHeader("Content-Disposition", 'attachment; filename=data.csv')
+
+      res$body <- capture.output(write.csv(val, stdout(), row.names = FALSE)) %>% 
+                  paste0(collapse="\n")
+
+      return(res$toResponse())
+    }, error=function(e){
+      errorHandler(req, res, e)
+    })
+  }
+}
+
+try({
+    addSerializer("csv", serializer_csv)
+})
 
 api <- plumb('./api.R')
 api$run(port = 8000)
